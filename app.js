ebebd3a0a3bd454c6c244b2a10eab8620da1165a
Felipe Acevedo <facevedom@unal.edu.co>
2016-11-26 15:29:21 -0500

    Conversation bindeada

--- app.js
+++ app.js
@@ -15,6 +15,7 @@
  */
 
 'use strict';
+console.log("hola");
 
 var express    = require('express'),
   app          = express(),
@@ -31,8 +32,14 @@ require('./config/express')(app);
 // Create the service wrapper
 var personalityInsights = watson.personality_insights({
   version: 'v2',
-  username: '<username>',
-  password: '<password>'
+  username: '5e53bade-d01f-4beb-bcf6-de1dbf8186f8',
+  password: '4YwXuTIuUqmu'
+});
+var conversation = watson.conversation({
+   version: 'v1',
+   version_date: '2016-09-20',
+   username: '54a07256-2385-48fb-9add-5be941734f97',
+   password: 'ZX31F0bGaLGo'
 });
 
 app.get('/', function(req, res) {
