bf82537cf89019d48f1cb19062dec7c4cc63d34f
paulddrix <paulddrix@protonmail.com>
2016-11-27 09:39:37 -0500

    cambie .env, edited gulpfile, y la visat de dashboard

--- gulpfile.js
+++ gulpfile.js
@@ -13,19 +13,20 @@ if(!process.env.PORT){
 }
 // startup required services to run the app server
 gulp.task('mongod', () => { 
-    // spawn in a child process mongodb
+	// spawn in a child process mongodb
     child_process.exec('mongod', (err,stdout,stderr) => {
     	console.log(stdout);
     });
 });
 // Create keys and store them in the right folder
 gulp.task('genRSAKeys', () => { 
-  exec('openssl genrsa -out ./keys/private.pem 1024 && openssl rsa -in ./keys/private.pem -pubout > ./keys/public.pub', function(err,stdout,stderr){
+	exec('openssl genrsa -out ./keys/private.pem 1024 && openssl rsa -in ./keys/private.pem -pubout > ./keys/public.pub', function(err,stdout,stderr){
     console.log(stdout);
   });
 });
 gulp.task('envFile', () => { 
-  exec('echo \"DEBUG=True\" > ./.env', (err,stdout,stderr) => {
+	exec('echo \"DEBUG=True\" \"MONGODB_URI=mongodb://admin:WALBWWZERVWLMHRR@sl-us-dal-9-portal.3.dblayer.com:17156,sl-us-dal-9-portal.4.dblayer.com:17156/admin?ssl=true\"
+	\"WORKSPACE_ID=e31d19f8-8401-4a2f-87a9-42618c14d017\"> ./.env', (err,stdout,stderr) => {
     console.log(stdout);
   });
 });
